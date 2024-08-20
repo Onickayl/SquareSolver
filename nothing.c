45b43d0ba71d0707cb424e3f40b679fca013771e
Onickayl <elizabetafox@yandex.ru>
2024-08-20 16:49:01 +0300

    add const

--- nothing.c
+++ nothing.c
@@ -2,42 +2,14 @@
 #include <math.h>
 #include <TXLib.h>
 
-int SolverSquare(double a, double b, double c, double* x1, double* x2)
-{
-    double D = 0;
-    D = b * b - 4 * a * c;
-    if (a == 0)
-    {
-        if (b == 0)
-        {
-            if (c == 0)
-                return -1;
-            else
-                return 0;
-        }
-        else
-        {
-            *x1 = -c / b;
-            return 1;
-        }
-    }
-    else
-    {
-            if (D < 0)
-                return 0;
-            else if (D == 0)
-            {
-                *x1 = -b / (2 * a);
-                return 1;
-            }
-            else
-            {
-                *x1 = (-b + sqrt(D)) / (2 * a);
-                *x2 = (-b - sqrt(D)) / (2 * a);
-                return 2;
-            }
-    }
-}
+//решить проблему с предупреждениями, числа с плавующей запятой, погрешность
+
+const int Many_Roots = -1;
+const int One_Root = 1;
+const int Two_Roots = 2;
+const int No_Roots = 0;
+
+int SolveSquare(double a, double b, double c, double* x1, double* x2);
 
 int main(void)
 {
@@ -53,20 +25,57 @@ int main(void)
 
 
     double x1 = 0, x2 = 0;
-    int nRoots = SolverSquare(a, b, c, &x1, &x2);
+    int nRoots = SolveSquare(a, b, c, &x1, &x2);
 
     switch(nRoots)
     {
-        case 0: printf("Answer: no roots\n");
+        case No_Roots: printf("Answer: no roots\n");
                 break;
-        case 1: printf("Answer: x = %lg\n", x1);
+        case One_Root: printf("Answer: x = %lg\n", x1);
                 break;
-        case 2: printf("Answer: x1 = %lg, x2 = %lg\n", x1, x2);
+        case Two_Roots: printf("Answer: x1 = %lg, x2 = %lg\n", x1, x2);
                 break;
-        case -1: printf("Answer: any numders\n");
+        case Many_Roots: printf("Answer: any numders\n");
                 break;
         default: printf("main(): ERROR: nRoots = %d\n", nRoots);
                 return 1;
     }
     return 0;
+}
+
+int SolveSquare(double a, double b, double c, double* x1, double* x2)
+{
+    double D = 0;
+    D = b * b - 4 * a * c;
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+                return Many_Roots;
+            else
+                return No_Roots;
+        }
+        else
+        {
+            *x1 = -c / b;
+            return One_Root;
+        }
+    }
+    else
+    {
+            if (D < 0)
+                return No_Roots;
+            else if (D == 0)
+            {
+                *x1 = -b / (2 * a);
+                return One_Root;
+            }
+            else
+            {
+                *x1 = (-b + sqrt(D)) / (2 * a);
+                *x2 = (-b - sqrt(D)) / (2 * a);
+                return Two_Roots;
+            }
+    }
 }
