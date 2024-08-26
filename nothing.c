ce6a325e5f304067f410549fe3b0f34090dfc495
Onickayl <elizabetafox@yandex.ru>
2024-08-26 12:49:26 +0300

    add size

--- nothing.c
+++ nothing.c
@@ -288,9 +288,10 @@ AllTests_Status All_Tests()
                                             {3, 1, 3, -70, 7, -10, 2}, {4, 5, -8, -4, -0.4, 2, 2},
                                             {5, 3, 4, 20, 0, 0, No_Roots}, {6, 4, 4, 1, -0.5, 0, 1},
                                             {7, 0, 0, 0, 0, 0, Many_Roots}, {8, 1, 5, 6, -3, -2, 2},
-                                            {9, 1, 2, 1, -1, 9, 1}, {10, -3, -1, 14, -7.0/3, 2, 2}};   // 9 заменить на 0
+                                            {9, 1, 2, 1, -1, 0, 1}, {10, -3, -1, 14, -7.0/3, 2, 2}};
 
-    for (unsigned int i = 0; i < sizeof(Array_Of_Structures) / sizeof(Array_Of_Structures[0]); i++)
+    size_t size = sizeof(Array_Of_Structures) / sizeof(Array_Of_Structures[0]);
+    for (size_t i = 0; i < size; i++)
     {
         if (UnitTest(&Array_Of_Structures[i]) == Test_Failure)
             return AllTests_Failure;
